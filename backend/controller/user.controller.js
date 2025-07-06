06a1e896b2a58ba064dda8ac99af050381ea06f5
Prince Kumar <princekumarkha2005@gmail.com>
2025-07-06 14:39:18 +0530

    first commit

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,11 @@
+export const signup=(req,res)=>{
+    res.send("User signup endpoint");  
+}
+
+export const login=(req,res)=>{
+    res.send("User login endpoint");
+}
+
+export const logout=(req,res)=>{
+    res.send("user Logout endpoint");
+}
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import userRoutes from './routes/user.route.js' // Import user routes
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3000
+
+//mongodb Connection
+mongoose.connect(process.env.MONGO_URI, {
+}) .then(() => {
+  console.log('MongoDB connected successfully') 
+}).catch((err) => {
+  console.error('MongoDB connection error:', err)
+})
+
+//routes
+
+app.use(express.json()) // Middleware to parse JSON requests
+app.use('/api/v1/user', userRoutes) // Use user routes
+
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
